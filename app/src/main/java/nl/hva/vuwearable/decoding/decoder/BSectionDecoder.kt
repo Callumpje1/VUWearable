804083855f780a821cb9a4b7dc85597599919c0b
svadkoc <SvadBellum1997!>
2022-12-14 10:35:11 +0100

    Decoder
    BSection decoder to seperate into sections and convert to bytes

--- app/src/main/java/nl/hva/vuwearable/decoding/decoder/BSectionDecoder.kt
+++ app/src/main/java/nl/hva/vuwearable/decoding/decoder/BSectionDecoder.kt
@@ -3,6 +3,8 @@ package nl.hva.vuwearable.decoding.decoder
 import android.nfc.Tag
 import android.util.Log
 import nl.hva.vuwearable.decoding.PacketDecoding
+import nl.hva.vuwearable.decoding.getInt
+import nl.hva.vuwearable.decoding.models.ASection
 import nl.hva.vuwearable.decoding.models.BSection
 import java.nio.ByteBuffer
 import java.util.*
@@ -13,7 +15,6 @@ class BSectionDecoder: PacketDecoding<Map<Int, BSection>> {
 
         const val B_FIRST_BYTE: Byte  = 42
         const val B_SECOND_BYTE: Byte  = 12
-
 
         const val B_PART_LENGTH = 12
 
@@ -48,17 +49,64 @@ class BSectionDecoder: PacketDecoding<Map<Int, BSection>> {
                 array.add(byte)
             }
 
-            Log.i("hallo",array.toString())
+            Log.i("parse",array.toString())
         }
 
         return separateIntoSections(array)
     }
 
+
     override fun separateIntoSections(array: LinkedList<Byte>): LinkedHashMap<Int, ByteArray> {
-        TODO("Not yet implemented")
+        val map = LinkedHashMap<Int, ByteArray>()
+
+        var currentStart = 0
+
+        /*
+         Till the end of the array, split up all the 'A's into its own section.
+         Example:
+         (65 char code == 'A')
+         List: [65, 49 ,45, 65, 34, 98]
+         Into: 0: [65, 49, 45], 1: [65, 34, 98]
+         */
+        while (currentStart + B_PART_LENGTH <= array.size - 1) {
+            val subList = array.subList(currentStart, currentStart + B_PART_LENGTH)
+            map[map.size] = subList.toByteArray()
+            currentStart += B_PART_LENGTH
+        }
+        Log.i("seperate",map.toString())
+
+        return map
     }
 
     override fun convertBytes(array: ByteArray, byteBuffer: ByteBuffer): Map<Int, BSection> {
-        TODO("Not yet implemented")
+        val parsedSections = parsePacket(array)
+        val results = mutableMapOf<Int, BSection>()
+
+        parsedSections.values.forEachIndexed { index, sectionArray ->
+            // Get tick count section
+            val tickCountArray = byteArrayOf(
+                sectionArray[4],
+                sectionArray[5],
+                sectionArray[6],
+                sectionArray[7]
+            )
+
+            // Get ICG section
+            val icgArray = byteArrayOf(
+                sectionArray[12],
+                sectionArray[13],
+                sectionArray[14],
+                sectionArray[15]
+            )
+
+            // Put the result in the map with the corresponding values
+            results[index] = BSection(
+                byteBuffer.getInt(tickCountArray),
+                BATTERY_FORMULA(byteBuffer.getInt(icgArray))
+            )
+        }
+        Log.i("convert",results.toString())
+
+        return results
     }
 }
